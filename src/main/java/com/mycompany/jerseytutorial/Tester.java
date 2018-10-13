1d304a1f9f08e7334c9d0771eaf6ddcc11e12a87
Shaner125 <x15745561@student.ncirl.ie>
2018-10-13 18:21:38 +0100

    Initial push, roman converter set up and working

---
+++ src/main/java/com/mycompany/jerseytutorial/Tester.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.jerseytutorial;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam; 
+import javax.ws.rs.core.Response;
+					
+@Path("/RomanConverter") 
+public class Tester {
+					
+  @GET
+  @Path("/{param}")
+  public Response sayHelloWorld(@PathParam("param") String message) {
+					
+    String output = message+" converted to roman numerals = "+RomanNumerals(Integer.parseInt(message)) + "!";
+					
+    return Response.status(200).entity(output).build(); 
+  }
+  
+  public static String RomanNumerals(int Int) {
+    LinkedHashMap<String, Integer> roman_numerals = new LinkedHashMap<String, Integer>();
+    roman_numerals.put("M", 1000);
+    roman_numerals.put("CM", 900);
+    roman_numerals.put("D", 500);
+    roman_numerals.put("CD", 400);
+    roman_numerals.put("C", 100);
+    roman_numerals.put("XC", 90);
+    roman_numerals.put("L", 50);
+    roman_numerals.put("XL", 40);
+    roman_numerals.put("X", 10);
+    roman_numerals.put("IX", 9);
+    roman_numerals.put("V", 5);
+    roman_numerals.put("IV", 4);
+    roman_numerals.put("I", 1);
+    String res = "";
+    
+    for(Map.Entry<String, Integer> entry : roman_numerals.entrySet()){
+      int matches = Int/entry.getValue();
+      res += repeat(entry.getKey(), matches);
+      Int = Int % entry.getValue();
+    }
+    return res;
+  }
+  public static String repeat(String s, int n) {
+    if(s == null) {
+        return null;
+    }
+    final StringBuilder sb = new StringBuilder();
+    for(int i = 0; i < n; i++) {
+        sb.append(s);
+    }
+    return sb.toString();
+  }
+					
+}
+
